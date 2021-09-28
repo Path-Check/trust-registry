2c5ee672026c54c456885d36b2b140d363149ee1
Vitor Pamplona <vitor@vitorpamplona.com>
2021-09-28 16:54:35 -0400

    Error handling

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const port = process.env.PORT || "8000";
 
 // Trust Over IP Trust Registry Protocol (RESTful API)
 
+/**
+ *  Data Model: 
+ *
+ *  identifier:             The URI-based identifier of a DID or X.509 Issuer or Verifier. 
+ *  credentialType:         String
+ *  governanceFrameworkURI: The URI that points to the Ecosystem Governance Framework.
+ *  DIDDocument:            Provides a URI that resolves to the DID Document for the Identifier. 
+ *  entityType:             One of [issuer, verifier, trustregistry]
+ *  status:                 One of [current, expired, terminated, revoked]
+ *  validFromDT:            date-time
+ *  validUntilDT:           date-time
+ */
+
 const DBIssuers = {
     "SmartHealthCards": {
         "https://myvaccinerecord.cdph.ca.gov/creds": {
@@ -39,48 +52,68 @@ const DBIssuers = {
 const DBVerifiers = {}
 const DBTrustRegistries = {}
 
-/**
-    Data Model: 
+function filter(DB, req, res) {
+    if (!DB[req.query.governanceFrameworkURI]) {
+        res.status(404).send({
+            title: "The Governance framework " + req.query.governanceFrameworkURI + " was not found",
+            status: 404
+        })
+        return undefined;
+    }
 
-    DIDDocument:                 // Provides a URI that resolves to the DID Document for the Identifier. 
-    entityType:                  // One of [issuer, verifier, trustregistry]
-    status:                      // One of [current, expired, terminated, revoked]
-    validFromDT:                 // date-time
-    validUntilDT:                // date-time
- */
+    if (!DB[req.query.governanceFrameworkURI][req.query.identifier]) {
+        res.status(404).send({
+            title: "Identifier " + req.query.identifier + " is not registered under " + req.query.governanceFrameworkURI,
+            status: 404
+        })
+        return undefined;
+    }
+
+    if (!DB[req.query.governanceFrameworkURI][req.query.identifier][req.query.credentialType]) {
+        res.status(404).send({
+            title: "Identifier " + req.query.identifier + " is not approved for the credential type " + req.query.credentialType,
+            status: 404
+        })
+        return undefined;
+    }
+
+    return DB[req.query.governanceFrameworkURI][req.query.identifier][req.query.credentialType];
+} 
 
 /* 
  * Allows querying to determine the status of an Issuer, as identified by their Identifier (unique), credential type, 
  * and EGF that they are operating under.
+ *
+ * Params: 
+ *  req.query.identifier:             The URI-based identifier of a DID or X.509 Issuer or Verifier. 
+ *  req.query.credentialType:         String
+ *  req.query.governanceFrameworkURI: The URI that points to the Ecosystem Governance Framework.
  */
 app.get('/query/issuer', async function (req, res) {
-    console.log(req.query.identifier);              // The URI-based identifier of a DID or X.509 Issuer. 
-    console.log(req.query.credentialType);          // String
-    console.log(req.query.governanceFrameworkURI);  // The URI that points to the Ecosystem Governance Framework.
+    const issuer = filter(DBIssuers, req, res);
 
-    const issuer = DBIssuers[req.query.governanceFrameworkURI][req.query.identifier][req.query.credentialType];
+    if (issuer) {
+        let response = {
+            identifier: req.query.identifier,
+            credentialType: req.query.credentialType,
+            governanceFrameworkURI: req.query.governanceFrameworkURI,
+            ... issuer
+        }
 
-    console.log(req.query.credentialType);
-
-    let response = {
-        identifier: req.query.identifier,
-        credentialType: req.query.credentialType,
-        governanceFrameworkURI: req.query.governanceFrameworkURI,
-        ... issuer
+        res.send(response);
     }
-
-    res.send(response);
 });
 
 /* 
  * Allows querying to determine the status of an Verifier, as identified by their Identifier (unique), credential type, 
  * and EGF that they are operating under.
+ *
+ * Params: 
+ *  req.query.identifier:             The URI-based identifier of a DID or X.509 Issuer or Verifier. 
+ *  req.query.credentialType:         String
+ *  req.query.governanceFrameworkURI: The URI that points to the Ecosystem Governance Framework.
  */
 app.get('/query/verifier', async function (req, res) {
-    console.log(req.query.identifier);             // The URI-based identifier of a DID or X.509 Verifier. 
-    console.log(req.query.credentialType);         // String
-    console.log(req.query.governanceFrameworkURI); // The URI that points to the Ecosystem Governance Framework.
-
     // all verifiers are allowed. 
     let response = {
         identifier: req.query.identifier,
@@ -97,22 +130,25 @@ app.get('/query/verifier', async function (req, res) {
 /* 
  * Allows querying to get an answer if this Trust Registry has a trust relationship with the identified Trust Registry. 
  * If one exists the returned status provides indicator of the trust relationship. 
+ *
+ * Params: 
+ *  req.query.identifier:             The URI-based identifier of a DID or X.509 Issuer or Verifier. 
+ *  req.query.credentialType:         String
+ *  req.query.governanceFrameworkURI: The URI that points to the Ecosystem Governance Framework.
  */
 app.get('/query/trustregistry', async function (req, res) {
-    console.log(req.query.identifier);              // The URI-based identifier of a DID or X.509 Issuer or Verifier. 
-    console.log(req.query.credentialType);          // String
-    console.log(req.query.governanceFrameworkURI);  // The URI that points to the Ecosystem Governance Framework.
+    const registry = filter(DBTrustRegistries, req, res);
 
-    const issuer = DBTrustRegistries[req.query.governanceFrameworkURI][req.query.identifier][req.query.credentialType];
+    if (registry) {
+        let response = {
+            identifier: req.query.identifier,
+            credentialType: req.query.credentialType,
+            governanceFrameworkURI: req.query.governanceFrameworkURI,
+            ... registry
+        }
 
-    let response = {
-        identifier: req.query.identifier,
-        credentialType: req.query.credentialType,
-        governanceFrameworkURI: req.query.governanceFrameworkURI,
-        ... issuer
+        res.send(response);
     }
-
-    res.send(response);
 });
 
 /* 
