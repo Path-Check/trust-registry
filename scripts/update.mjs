71cc2d649e870ef29cdb8793a8c26b9dd1c87054
Vitor Pamplona <vitor@vitorpamplona.com>
2021-10-23 10:41:49 -0400

    Removing old migration code.

--- scripts/update.mjs
+++ scripts/update.mjs
@@ -8,39 +8,6 @@ let registry = JSON.parse(fs.readFileSync("registry.json"));
 registry = await DCC.update(registry);
 registry = await VCI.update(registry);
 
-/*
-let registry2 = {
-  "CRED": registry["CRED"],
-  "ICAO": registry["ICAO"], 
-  "DIVOC": registry["DIVOC"],
-  "EUDCC": registry["EUDCC"]
-};
-
-const standard = "SmartHealthCards";
-
-registry2[standard] = {};
-for (const issuer of Object.keys(registry[standard])) {
-  let entry = registry[standard][issuer];
-  for (const key of entry.didDocument.jwks.keys) {
-    
-
-    let newEntry = {
-      "displayName": entry.displayName,
-      "entityType":  entry.entityType,
-      "status":  entry.status,
-      "validFromDT":  entry.validFromDT,
-      "didDocument": key,
-      "credentialType": entry.credentialType
-    };
-
-    if (!registry2[standard][issuer+"#"+key.kid])
-      registry2[standard][issuer+"#"+key.kid] = newEntry;
-    else 
-      console.log(key.kid, "already exists");
-  }
-}
-
-fs.writeFile('registry.json', JSON.stringify(registry2, null, 2), function writeJSON(err) {
+fs.writeFile('registry.json', JSON.stringify(registry, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
 });
-*/
