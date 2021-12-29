df024e93901cfb450e08c05608b9d2d6d8f812d8
Vitor Pamplona <vitor@vitorpamplona.com>
2021-12-29 12:40:48 -0500

    Not exporting invalid public keys

--- scripts/flatten.mjs
+++ scripts/flatten.mjs
@@ -8,20 +8,22 @@ let registry = JSON.parse(fs.readFileSync("registry_normalized.json"));
 let csv = []
 Object.entries(registry).forEach(([framework,v]) => {
   Object.entries(registry[framework]).forEach(([kid,v]) => {
-    csv.push([
-      framework,
-      kid,
-      v.entityType,
-      v.status,
-      v.displayName.en ? Buffer.from(v.displayName.en).toString('base64') : "",
-      v.displayLogo ? Buffer.from(v.displayLogo).toString('base64') : "",
-      v.validFromDT,
-      v.validUntilDT,
-      v.publicKey
-    ].join(","));
+    if (v.publicKey) {
+      csv.push([
+        framework,
+        kid,
+        v.entityType,
+        v.status,
+        v.displayName.en ? Buffer.from(v.displayName.en).toString('base64') : "",
+        v.displayLogo ? Buffer.from(v.displayLogo).toString('base64') : "",
+        v.validFromDT,
+        v.validUntilDT,
+        v.publicKey
+      ].join(","));
+    }
   }); 
 }); 
 
-fs.writeFile('registry.csv', csv.join("\n"), function writeCSV(err) {
+fs.writeFile('registry_normalized.csv', csv.join("\n"), function writeCSV(err) {
   if (err) return console.log(err);
 });
