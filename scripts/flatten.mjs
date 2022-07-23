da398deee118aeb2ba5e8959d9cb1e07e81fe11f
Vitor Pamplona <vitor@vitorpamplona.com>
2022-07-22 21:28:26 -0400

    Adding a test registry

--- scripts/flatten.mjs
+++ scripts/flatten.mjs
@@ -3,28 +3,33 @@ import * as VCI from './update_vci.mjs'
 
 import fs from 'fs';
 
-let registry = JSON.parse(fs.readFileSync("registry_normalized.json"));
+function flatten(registry_file, out_file) {
+  let registry = JSON.parse(fs.readFileSync(registry_file));
 
-let csv = []
-for (let framework in registry) {
-  for (let kid in registry[framework]) {
-    let v = registry[framework][kid]
-    if (v.publicKey) {
-      csv.push([
-        framework,
-        kid,
-        v.status,
-        v.displayName.en ? Buffer.from(v.displayName.en).toString('base64') : "",
-        v.displayLogo ? Buffer.from(v.displayLogo).toString('base64') : "",
-        v.validFromDT,
-        v.validUntilDT,
-        v.publicKey,
-        v.displayURL ? Buffer.from(v.displayURL).toString('base64') : "",
-      ].join(","));
-    }
+  let csv = []
+  for (let framework in registry) {
+    for (let kid in registry[framework]) {
+      let v = registry[framework][kid]
+      if (v.publicKey) {
+        csv.push([
+          framework,
+          kid,
+          v.status,
+          v.displayName.en ? Buffer.from(v.displayName.en).toString('base64') : "",
+          v.displayLogo ? Buffer.from(v.displayLogo).toString('base64') : "",
+          v.validFromDT,
+          v.validUntilDT,
+          v.publicKey,
+          v.displayURL ? Buffer.from(v.displayURL).toString('base64') : "",
+        ].join(","));
+      }
+    } 
   } 
-} 
 
-fs.writeFile('registry_normalized.csv', csv.join("\n"), function writeCSV(err) {
-  if (err) return console.log(err);
-});
+  fs.writeFile(out_file, csv.join("\n"), function writeCSV(err) {
+    if (err) return console.log(err);
+  });
+}
+
+flatten("registry_normalized.json", "registry_normalized.csv");
+flatten("test_registry_normalized.json", "test_registry_normalized.csv");
--- scripts/normalize.mjs
+++ scripts/normalize.mjs
@@ -3,45 +3,49 @@ import jwkToPem from 'jwk-to-pem'
 
 import fs from 'fs';
 
-let registry_source = JSON.parse(fs.readFileSync("registry.json"));
+function normalize(registry_file, out_file) {
+  let registry_source = JSON.parse(fs.readFileSync(registry_file));
 
-let registry = {} 
-// Rename frameworks 
-registry["CRED"] = registry_source["CRED"] 
-registry["ICAO"] = registry_source["ICAO"] 
-registry["DIVOC"] = registry_source["DIVOC"] 
-registry["DCC"] = registry_source["EUDCC"] 
-registry["SHC"] = registry_source["SmartHealthCards"] 
+  let registry = {} 
+  // Rename frameworks 
+  registry["CRED"] = registry_source["CRED"] 
+  registry["ICAO"] = registry_source["ICAO"] 
+  registry["DIVOC"] = registry_source["DIVOC"] 
+  registry["DCC"] = registry_source["EUDCC"] 
+  registry["SHC"] = registry_source["SmartHealthCards"] 
 
-function cleanPEM(pem) {
-  return pem.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replace(/\n/g, "").replace(/\r/g, "")
+  function cleanPEM(pem) {
+    return pem.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replace(/\n/g, "").replace(/\r/g, "")
+  }
+
+  for (let framework in registry) {
+    for (let kid in registry[framework]) {
+      let v = registry[framework][kid]
+      
+      try {
+        if (typeof (v.didDocument) === "string") {
+          if (v.didDocument.includes("CERTIFICATE"))
+            v['publicKey'] = cleanPEM(Certificate.fromPEM(v.didDocument).publicKey.toPEM())
+          else if (v.didDocument.includes("PUBLIC KEY"))
+            v['publicKey'] = cleanPEM(v.didDocument)
+          else
+            console.log("Something is wrong with:" + kid) 
+        } else {  
+          v['publicKey'] = cleanPEM(jwkToPem(v.didDocument));
+        }
+      } catch(e) {
+        console.log(v.didDocument)
+        console.log(e)
+      }
+      
+      delete v.didDocument
+    } 
+  } 
+
+  fs.writeFile(out_file, JSON.stringify(registry, null, 2), function writeJSON(err) {
+    if (err) return console.log(err);
+  });
 }
 
-for (let framework in registry) {
-  for (let kid in registry[framework]) {
-    let v = registry[framework][kid]
-    
-    try {
-      if (typeof (v.didDocument) === "string") {
-        if (v.didDocument.includes("CERTIFICATE"))
-          v['publicKey'] = cleanPEM(Certificate.fromPEM(v.didDocument).publicKey.toPEM())
-        else if (v.didDocument.includes("PUBLIC KEY"))
-          v['publicKey'] = cleanPEM(v.didDocument)
-        else
-          console.log("Something is wrong with:" + kid) 
-      } else {  
-        v['publicKey'] = cleanPEM(jwkToPem(v.didDocument));
-      }
-    } catch(e) {
-      console.log(v.didDocument)
-      console.log(e)
-    }
-    
-    delete v.didDocument
-  } 
-} 
-
-fs.writeFile('registry_normalized.json', JSON.stringify(registry, null, 2), function writeJSON(err) {
-  if (err) return console.log(err);
-});
-
+normalize("registry.json", "registry_normalized.json");
+normalize("test_registry.json", "test_registry_normalized.json");
